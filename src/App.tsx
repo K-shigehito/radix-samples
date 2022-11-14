440cdae53d61574abc91278ce7bf46fbdaeaa580
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-11-14 23:08:46 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export const App = () => {
+  const test = () => {
+    console.log('test')
+  }
+  test()
+  return (
+    <div className="flex gap-4 bg-slate-100 p-4 text-lg">
+      <div>test</div>
+      <div>test</div>
+    </div>
+  )
+}
+
+export default App
